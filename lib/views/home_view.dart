431d42febd5dc360734078e6ca3d2fe0d34dea52
zoniha <awaawapurin@gmail.com>
2021-07-15 22:28:42 +0900

    ✨feat: Done

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -10,6 +10,9 @@ class _HomeViewState extends State<HomeView> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final FirebaseAuth auth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  String errorMessage = '';
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -19,54 +22,143 @@ class _HomeViewState extends State<HomeView> {
       appBar: AppBar(
         title: Text('Auth User (Logged ' + (user == null ? 'out' : 'in') + ')'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextField(
-                controller: emailController,
-              ),
-              TextField(
-                controller: passwordController,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    child: Text('Sign Up'),
-                    onPressed: () async {
-                      await auth.createUserWithEmailAndPassword(
-                        email: emailController.text,
-                        password: passwordController.text,
-                      );
-                      setState(() {});
-                    },
+      body: Form(
+        key: _key,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextFormField(
+                  controller: emailController,
+                  validator: validationEmail,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  validator: validationPassword,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Center(
+                    child: Text(
+                      errorMessage,
+                      style: TextStyle(
+                        color: Colors.pinkAccent,
+                      ),
+                    ),
                   ),
-                  ElevatedButton(
-                    child: Text('Sign In'),
-                    onPressed: () async {
-                      await auth.signInWithEmailAndPassword(
-                        email: emailController.text,
-                        password: passwordController.text,
-                      );
-                      setState(() {});
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('Log Out'),
-                    onPressed: () async {
-                      await auth.signOut();
-                      setState(() {});
-                    },
-                  ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      child: _isLoading
+                          ? CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : Text('Sign Up'),
+                      onPressed: user != null
+                          ? null
+                          : () async {
+                              setState(() {
+                                _isLoading = true;
+                                errorMessage = '';
+                              });
+                              if (_key.currentState!.validate()) {
+                                try {
+                                  await auth.createUserWithEmailAndPassword(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                  );
+                                  errorMessage = '';
+                                } on FirebaseAuthException catch (error) {
+                                  errorMessage = error.message!;
+                                }
+                                setState(() => _isLoading = false);
+                              }
+                            },
+                    ),
+                    ElevatedButton(
+                      child: _isLoading
+                          ? CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : Text('Sign In'),
+                      onPressed: user != null
+                          ? null
+                          : () async {
+                              setState(() {
+                                _isLoading = true;
+                                errorMessage = '';
+                              });
+                              if (_key.currentState!.validate()) {
+                                try {
+                                  await auth.signInWithEmailAndPassword(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                  );
+                                  errorMessage = '';
+                                } on FirebaseAuthException catch (error) {
+                                  errorMessage = error.message!;
+                                }
+                                setState(() => _isLoading = false);
+                              }
+                            },
+                    ),
+                    ElevatedButton(
+                      child: _isLoading
+                          ? CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : Text('Log Out'),
+                      onPressed: user == null
+                          ? null
+                          : () async {
+                              try {
+                                setState(() {
+                                  _isLoading = true;
+                                  errorMessage = '';
+                                });
+                                await auth.signOut();
+                              } on FirebaseAuthException catch (error) {
+                                errorMessage = error.message!;
+                              }
+                              setState(() => _isLoading = false);
+                            },
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+String? validationEmail(String? formEmail) {
+  if (formEmail == null || formEmail.isEmpty) return 'Email is required.';
+
+  final String pattern = r'\w+@\w+\.\w+';
+  final RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formEmail)) return 'Invalid Email';
+
+  return null;
+}
+
+String? validationPassword(String? formPassword) {
+  if (formPassword == null || formPassword.isEmpty) return 'Password is required.';
+
+  final String pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+  final RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formPassword))
+    return '''
+      Password must be at least 8 characters,
+      include an uppercase letter, number and symbol.
+      ''';
+
+  return null;
 }
